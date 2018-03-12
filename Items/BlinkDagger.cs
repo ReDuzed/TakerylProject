e3f75f04f5e98e5dfff4c9a2077ef057fcf0cc76
700hours <31488826+700hours@users.noreply.github.com>
2018-03-12 00:33:13 -0700

    Add files via upload
    Migrated code to items
    Lerp implemented in Blink
    Spin hitbox is better

--- Items/BlinkDagger.cs
+++ Items/BlinkDagger.cs
@@ -1,50 +1,56 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.ModLoader.IO;
-using Terraria.GameInput;
 
-namespace TakerylProject
+namespace TakerylProject.Items
 {
-	public class ProjectPlayer : ModPlayer
+	public class BlinkDagger : ModItem
 	{
-		public bool canSpin = false;
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Blink Dagger");
+			Tooltip.SetDefault("Blink through space"
+					+	"\nHold 'Shift' and 'Move Left or Right' to charge");
+		}
+		public override void SetDefaults()
+		{
+			item.width = 28;
+			item.height = 28;
+			item.maxStack = 1;
+			item.value = 50000;
+			item.rare = 5;
+			item.scale = 1f;
+			item.accessory = true;
+		}
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(9);
+			recipe.SetResult(this, 1);
+			recipe.AddRecipe();
+		}
 		
-	/*	int blinkCharge = 0, blinkRange = 32, blinkDust, blinkCoolDown = 0;
+		int blinkCharge = 0, blinkRange = 64, blinkDust, blinkCoolDown = 0;
 		int radius, Radius; 
 		float degrees = 0, degrees2 = 0, degrees3 = 0;
 		bool canBlink = false, blinked;
 		bool facingRight, facingLeft;
-		const int defaultBlinkRange = 32;
+		float Depreciate = 30, Point;
+		const float Time = 30;
+		const int defaultBlinkRange = 64;
 		const int TileSize = 16;
 		Vector2 dustPos;
 		Vector2 center;
-		int spinCharge = 0, spinDuration = 600;
-		bool canSpin, spinning = false, charging = false;
-		int dmgTicks;
-		int soundTicks = 0;
-		bool thrown = false;
-		int throwCharge = 0, throwSpeed = 2;
-		int thrownWeapon = 0;
-		public int weaponType = 0;
-		Vector2 throwPos;
-		float CurrentPoint, Depreciating = 0; 
-		const float defaultThrowSpeed = 10;		
-		const float Time = 100;
-		const float gravity = 0.0612f;
-		Texture2D swordTexture;
-		public override void PreUpdate()
+		Vector2 OldPosition;
+		Vector2 Start, End;
+		public override void UpdateAccessory(Player player, bool hideVisual)
 		{
 			int TileX = (int)player.position.X/16;
 			int TileY = (int)player.position.Y/16;
-			if(!canSpin && !canBlink && !blinked && Main.GetKeyState((int)Microsoft.Xna.Framework.Input.Keys.LeftShift) < 0)
+			if(/*!canSpin && */!canBlink && !blinked && Main.GetKeyState((int)Microsoft.Xna.Framework.Input.Keys.LeftShift) < 0)
 			{
 				if((player.controlLeft || player.controlRight))
 				{
@@ -63,7 +69,7 @@ namespace TakerylProject
 				{
 					if(player.direction == 1)
 					{
-						for(int k = 0; k < blinkRange; k++)
+						for(int k = 0; k < defaultBlinkRange; k++)
 						{
 							if(CheckRight(TileX + blinkRange, TileY, player))
 								blinkRange -= 1;
@@ -73,7 +79,7 @@ namespace TakerylProject
 					}
 					else if(player.direction == -1)
 					{
-						for(int k = 0; k < blinkRange; k++)
+						for(int k = 0; k < defaultBlinkRange; k++)
 						{
 							if(CheckLeft(TileX - blinkRange, TileY, player))
 								blinkRange -= 1;
@@ -93,6 +99,13 @@ namespace TakerylProject
 						blinkCharge = 0;
 					}
 				}
+				// set start position
+				Start.X = player.position.X;
+				Start.Y = player.position.Y;
+				
+				// set end position
+				End.X = Start.X + TileSize * blinkRange;
+				End.Y = Start.Y;
 			}
 			if(canBlink)
 			{
@@ -166,24 +179,7 @@ namespace TakerylProject
 				if(blinkCharge >= 117)
 				{
 					Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Custom/teleport"), player.oldPosition);
-					if(facingRight)
-					{
-						player.position.X += TileSize*blinkRange;
-						facingRight = false;
-					}
-					else if(facingLeft)
-					{
-						player.position.X -= TileSize*blinkRange;
-						facingLeft = false;
-					}
-					for(int k = 0; k < 360/2; k++)
-					{
-						radius = 16;
-						blinkDust = Dust.NewDust(player.Center, 8, 8, 71, 0f, 0f, 0, Color.White, 1.2f);
-						Main.dust[blinkDust].noGravity = true;
-						Main.dust[blinkDust].velocity.X = (float)(radius*Math.Cos(k));
-						Main.dust[blinkDust].velocity.Y = (float)(radius*Math.Sin(k));
-					}
+					
 					blinkRange = defaultBlinkRange;
 					blinkCharge = 0;
 					blinkCoolDown = 360;
@@ -192,108 +188,39 @@ namespace TakerylProject
 					blinked = true;
 				}
 			}
+			if(blinked)
+			{
+				if(facingRight && Depreciate > 0)
+				{
+					Depreciate--;
+					Point = (Time - Depreciate) / Time;
+					player.position = Vector2.Lerp(Start, End, Point);
+				}
+				else if(facingLeft && Depreciate > 0)
+				{
+					Depreciate--;
+					Point = (Time - Depreciate) / Time;
+					player.position = Vector2.Lerp(Start, new Vector2(Start.X - TileSize * blinkRange, Start.Y), Point);
+				}
+				if(Depreciate == 0)
+				{
+					for(int k = 0; k < 360/2; k++)
+					{
+						radius = 16;
+						blinkDust = Dust.NewDust(player.Center, 8, 8, 71, 0f, 0f, 0, Color.White, 1.2f);
+						Main.dust[blinkDust].noGravity = true;
+						Main.dust[blinkDust].velocity.X = (float)(radius*Math.Cos(k));
+						Main.dust[blinkDust].velocity.Y = (float)(radius*Math.Sin(k));
+					}
+					blinked = false;
+					Depreciate = Time;
+				}
+			}
 			if(blinkCoolDown > 0) 
 				blinkCoolDown--;
 			if(blinkCoolDown == 0) 
 				blinked = false;
-			Item item = player.inventory[player.selectedItem];
-			if(!canSpin && !spinning && Main.mouseMiddle && item.type == 368)
-			{
-				spinCharge++;
-				if(spinCharge >= 60)
-				{
-					canSpin = true;
-					spinCharge = 0;
-					spinDuration = 600;
-				}
-				if(spinCharge == 30)
-					Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Custom/conjure"), player.position);
-			}
-			else
-			{
-				if(spinCharge > 0) 
-					spinCharge--;
-			}
-			if(canSpin)
-			{
-				blinkCharge = 0;
-				if(item.type == 368)
-				{
-					spinning = true;
-					
-					radius = 72;
-					degrees2 += 0.06f;
-					
-					center = player.position + new Vector2(player.width/2, player.height/2);
-					
-					float PosX = center.X + (float)(radius*Math.Cos(degrees2));
-					float PosY = center.Y + (float)(radius*Math.Sin(degrees2));	
-					
-					Vector2 swordPos = player.bodyPosition + new Vector2(PosX, PosY);
-					
-					Rectangle swordHitBox = new Rectangle((int)swordPos.X, (int)swordPos.Y, item.width, item.height);
-					NPC[] npc = Main.npc;
-					for(int k = 0; k < npc.Length-1; k++)
-					{
-						NPC n = npc[k];
-						Vector2 npcv = new Vector2(n.position.X, n.position.Y);
-						Rectangle npcBox = new Rectangle((int)npcv.X, (int)npcv.Y, n.width, n.height);
-						if(n.active && !n.friendly && !n.dontTakeDamage 
-						&& dmgTicks == 0 && swordHitBox.Intersects(npcBox))
-						{
-							n.StrikeNPC((int)(item.damage + player.meleeDamage), item.knockBack*3, player.direction,false,false);
-							dmgTicks = 10;
-						}
-					}
-					Projectile[] projectile = Main.projectile;
-					for(int l = 0; l < projectile.Length-1; l++)
-					{
-						Projectile n = projectile[l];
-						Vector2 projv = new Vector2(n.position.X, n.position.Y);
-						Rectangle projBox = new Rectangle((int)projv.X, (int)projv.Y, n.width, n.height);
-						if(n.active && swordHitBox.Intersects(projBox))
-						{
-							n.timeLeft = 0;
-						}
-					}
-					
-					soundTicks++;
-					if(soundTicks%16 == 0)
-						Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Custom/SpinQuiet"), player.bodyPosition + new Vector2(PosX, PosY));
-				}
-				if(spinDuration > 0) 
-					spinDuration--;
-				if(spinDuration == 0)
-				{
-					degrees2 = 0f;
-					canSpin = false;
-					spinning = false;
-				}
-			}
-			if(dmgTicks > 0) dmgTicks--;
-			if(!thrown && item.type == 368 && Main.mouseRight)
-			{
-				throwCharge++;
-				if(throwCharge == 90)
-				{
-					thrown = true;
-					throwCharge = 0;
-					Depreciating = Time;
-				}
-			}
-			if(thrown)
-			{
-				throwCharge++;
-				if(throwCharge == 600)
-				{
-					thrown = false;
-					throwCharge = 0;
-				}
-			}
-			if(Depreciating > 0) 
-				Depreciating--;
 		}
-		
 		public bool CheckLeft(int i, int j, Player player)
 		{
 			bool Active = Main.tile[i-1, j].active() || Main.tile[i-1, j+1].active() || Main.tile[i-1, j +2].active();
@@ -309,38 +236,6 @@ namespace TakerylProject
 			
 			if(Active && Solid) return true;
 			else return false;
-		}	*/
-		float degrees3 = 0;
-		Texture2D swordTexture;
-		public override void DrawEffects(PlayerDrawInfo drawinfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright)
-		{
-			PlayerHeadDrawInfo drawInfo = new PlayerHeadDrawInfo();
-			drawInfo.spriteBatch = Main.spriteBatch;
-			drawInfo.drawPlayer = player;
-			
-			int radius = 72;
-			if(/*thrown || */canSpin)
-				degrees3 += 0.06f;
-			else degrees3 = 0;
-			
-		//	CurrentPoint = (Time - Depreciating) / Time;
-			
-			SpriteEffects effects = SpriteEffects.None;
-			Vector2 origin = new Vector2((float)player.legFrame.Width * 0.5f, (float)player.legFrame.Height * 0.5f);
-			Vector2 bodyPosition = new Vector2((float)((int)(player.position.X - Main.screenPosition.X - (float)(player.bodyFrame.Width / 2) + (float)(player.width / 2))), (float)((int)(player.position.Y - Main.screenPosition.Y + (float)player.height - (float)player.bodyFrame.Height + 4f)));
-
-			float MoveX = origin.X + (float)(radius*Math.Cos(degrees3));
-			float MoveY = origin.Y + (float)(radius*Math.Sin(degrees3));
-			
-			Item item = player.inventory[player.selectedItem];
-			if(item.type == 368)
-				swordTexture = Main.itemTexture[368];
-			if(canSpin)
-			{
-				Main.spriteBatch.Draw(swordTexture, 
-					bodyPosition + player.bodyPosition + new Vector2(MoveX, MoveY), 
-					null, Color.White, (degrees3*3)*(-1) + 0.48f, origin, 1f, effects, 0f);
-			}
 		}
 	}
 }
