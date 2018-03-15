1b198f7179f1d2117615f1f05246ab315bb3b2c2
700hours <31488826+700hours@users.noreply.github.com>
2018-03-15 16:58:43 -0700

    v0.4
    - Added UI
    - Added more sounds to interact with abilities

--- Items/BlinkDagger.cs
+++ Items/BlinkDagger.cs
@@ -37,14 +37,25 @@ namespace TakerylProject.Items
 		}
 		
 		int blinkCharge = 0, blinkRange = 64, blinkDust, blinkCoolDown = 0;
-		int radius, Radius; 
-		float degrees = 0, degrees2 = 0, degrees3 = 0;
 		bool canBlink = false, blinked;
 		bool facingRight, facingLeft;
+		
+		int radius, Radius; 
+		float degrees = 0, degrees2 = 0, degrees3 = 0;
 		float Depreciate = 30, Point;
+		
+		bool airStrikeUsed = false;
+		bool canAirStrike = false, striking = false, midAir = false;
+		int strikeDust, strikeCharge, strikeRange = 32, StrikeCoolDown = 0, airStrike;
+		float WaveTimer = 0f;
+		int ticks = 0;
+		
 		const float Time = 30;
+		const float radians = 0.017f;
 		const int defaultBlinkRange = 64;
+		const int defaultStrikeRange = 32;
 		const int TileSize = 16;
+		
 		Vector2 dustPos;
 		Vector2 center;
 		Vector2 OldPosition;
@@ -55,9 +66,11 @@ namespace TakerylProject.Items
 			
 			int TileX = (int)player.position.X/16;
 			int TileY = (int)player.position.Y/16;
-			if(/*!canSpin && */!canBlink && !blinked && Main.GetKeyState((int)Microsoft.Xna.Framework.Input.Keys.LeftShift) < 0)
+			
+			#region Blink
+			if(modPlayer.blinkCoolDown == 0 && /*!canSpin && */!canBlink && !blinked && Main.GetKeyState((int)Microsoft.Xna.Framework.Input.Keys.LeftShift) < 0)
 			{
-				if((player.controlLeft || player.controlRight))
+				if(player.controlLeft || player.controlRight)
 				{
 					blinkCharge++;
 					if(blinkCharge == 1 || blinkCharge == 60)
@@ -118,6 +131,8 @@ namespace TakerylProject.Items
 			}
 			if(canBlink)
 			{
+				modPlayer.blinkCoolDown = 900;
+				
 				player.velocity = Vector2.Zero;
 				if(blinkCharge == 0)
 				{
@@ -234,6 +249,223 @@ namespace TakerylProject.Items
 				blinkCoolDown--;
 			if(blinkCoolDown == 0) 
 				blinked = false;
+			#endregion
+			
+			#region Air Strike
+			if(modPlayer.strikeCoolDown == 0 && StrikeCoolDown == 0 && !canAirStrike && Main.GetKeyState((int)Microsoft.Xna.Framework.Input.Keys.X) < 0)
+			{
+				if(!player.controlLeft && !player.controlRight)
+				{
+					strikeCharge++;
+				}
+				if(strikeCharge == 10 && !striking && !midAir )
+					Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Custom/ShineSparkLoop"), player.position);
+				for(int k = 0; k < defaultStrikeRange; k++)
+				{
+					if(CheckUp(TileX, TileY - strikeRange, player))
+						strikeRange--;
+				}
+				strikeCharge = 0;
+				canAirStrike = true;
+				
+				// set positions
+				Start = player.position;
+				End.X = Start.X;
+				End.Y = Start.Y - TileSize * strikeRange;
+			}
+			else if(StrikeCoolDown > 0 && !canAirStrike && Main.GetKeyState((int)Microsoft.Xna.Framework.Input.Keys.X) < 0)
+			{
+				ticks++;
+				if(ticks%20 == 0)
+					Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Custom/heathurt"), player.position);
+			}
+			if(canAirStrike && strikeRange >= 32 && !midAir && !striking)
+			{
+				StrikeCoolDown = 3600;
+				
+				player.velocity = Vector2.Zero;
+				if(strikeCharge == 10 && !striking && !midAir)
+					Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Custom/ChargeStartup"), player.position);
+				if(strikeCharge < 180)
+					strikeCharge++;
+				if(strikeCharge >= 117)
+				{
+					striking = true;
+					strikeCharge = 0;
+					Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Custom/teleport"), Start);
+					canAirStrike = false;
+				}
+				degrees+=0.06f;
+				
+				radius = 64;
+				double X = (radius*Math.Cos(radius*2/(180/Math.PI)));
+				double Y = (radius*Math.Sin(radius*2/(180/Math.PI)));
+				center = player.position + new Vector2(player.width/2, player.height/2);
+				strikeDust = Dust.NewDust(center, 8, 8, 71, 0f, 0f, 0, Color.White, 1.0f);
+				Main.dust[strikeDust].noGravity = true;
+				Main.dust[strikeDust].position.X = center.X + (float)(radius*Math.Cos(degrees));
+				Main.dust[strikeDust].position.Y = center.Y + (float)(radius*Math.Sin(degrees));
+				#region points on circle
+				if(strikeCharge >= 15)
+				{
+					int degree45 = Dust.NewDust(player.position + new Vector2((float)X, (float)Y), 8, 8, 71, 0f, 0f, 0, Color.White, 0.5f);
+					Main.dust[degree45].noGravity = false;
+					Main.dust[degree45].position.X = center.X + (float)(radius*Math.Cos(45));
+					Main.dust[degree45].position.Y = center.Y + (float)(radius*Math.Sin(45));
+				}
+				if(strikeCharge >= 30)
+				{
+					int degree90 = Dust.NewDust(player.position + new Vector2((float)X, (float)Y), 8, 8, 71, 0f, 0f, 0, Color.White, 0.5f);
+					Main.dust[degree90].noGravity = false;
+					Main.dust[degree90].position.X = center.X + (float)(radius*Math.Cos(90));
+					Main.dust[degree90].position.Y = center.Y + (float)(radius*Math.Sin(90));
+				}
+				if(strikeCharge >= 45)
+				{
+					int degree135 = Dust.NewDust(player.position + new Vector2((float)X, (float)Y), 8, 8, 71, 0f, 0f, 0, Color.White, 0.5f);
+					Main.dust[degree135].noGravity = false;
+					Main.dust[degree135].position.X = center.X + (float)(radius*Math.Cos(135));
+					Main.dust[degree135].position.Y = center.Y + (float)(radius*Math.Sin(135));
+				}
+				if(strikeCharge >= 60)
+				{
+					int degree180 = Dust.NewDust(player.position + new Vector2((float)X, (float)Y), 8, 8, 71, 0f, 0f, 0, Color.White, 0.5f);
+					Main.dust[degree180].noGravity = true;
+					Main.dust[degree180].position.X = center.X + (float)(radius*Math.Cos(180));
+					Main.dust[degree180].position.Y = center.Y + (float)(radius*Math.Sin(180));
+				}
+				if(strikeCharge >= 75)
+				{
+					int degree225 = Dust.NewDust(player.position + new Vector2((float)X, (float)Y), 8, 8, 71, 0f, 0f, 0, Color.White, 0.5f);
+					Main.dust[degree225].noGravity = true;
+					Main.dust[degree225].position.X = center.X + (float)(radius*Math.Cos(225));
+					Main.dust[degree225].position.Y = center.Y + (float)(radius*Math.Sin(225));
+				}
+				if(strikeCharge >= 90)
+				{
+					int degree270 = Dust.NewDust(player.position + new Vector2((float)X, (float)Y), 8, 8, 71, 0f, 0f, 0, Color.White, 0.5f);
+					Main.dust[degree270].noGravity = true;
+					Main.dust[degree270].position.X = center.X + (float)(radius*Math.Cos(270));
+					Main.dust[degree270].position.Y = center.Y + (float)(radius*Math.Sin(270));
+				}
+				if(strikeCharge >= 117)
+				{
+					int degree315 = Dust.NewDust(player.position + new Vector2((float)X, (float)Y), 8, 8, 71, 0f, 0f, 0, Color.White, 0.5f);
+					Main.dust[degree315].noGravity = true;
+					Main.dust[degree315].position.X = center.X + (float)(radius*Math.Cos(315));
+					Main.dust[degree315].position.Y = center.Y + (float)(radius*Math.Sin(315));
+				}
+				#endregion
+			}
+			else if(strikeRange < 32)
+			{
+				strikeRange = defaultStrikeRange;
+				strikeCharge = 0;
+				canAirStrike = false;
+			}
+			if(striking)
+			{
+				modPlayer.strikeCoolDown = 2700;
+				if(Depreciate > 0)
+				{
+					Depreciate--;
+					Point = (Time - Depreciate) / Time;
+					player.position = Vector2.Lerp(Start, End, Point);
+				}
+				if(Depreciate == 0)
+				{
+					center = player.position + new Vector2(player.width/2, player.height/2);
+					
+					// need to update dust type
+					float radCircle = 0.017f;
+					for(int k = 0; k < 360/2; k++)
+					{
+						int r = 48;
+						strikeDust = Dust.NewDust(player.position + new Vector2(player.width/2, player.height/2), 8, 8, 20, 0f, 0f, 0, Color.White, 1f);
+						Main.dust[strikeDust].noGravity = true;
+						radCircle = 0.017f*(k*2);
+						Main.dust[strikeDust].velocity.X = (float)(r*Math.Cos(radCircle));
+						Main.dust[strikeDust].velocity.Y = (float)(r*Math.Sin(radCircle));
+					}
+					for(int l = 90; l < 270; l++)
+					{
+						int r = 48;
+						strikeDust = Dust.NewDust(player.position + new Vector2(player.width/2, player.height/2), 8, 8, 20, 0f, 0f, 0, Color.White, 1f);
+						Main.dust[strikeDust].noGravity = true;
+						radCircle = 0.017f*(l*2);
+						Main.dust[strikeDust].velocity.X = (float)(r*Math.Cos(radCircle));
+						Main.dust[strikeDust].velocity.Y = (float)(r*Math.Sin(radCircle));
+					}
+					radius = 16;
+					degrees = radians*45;
+					striking = false;
+					midAir = true;
+					Depreciate = Time;
+				}
+			}
+			if(midAir)
+			{
+				strikeCharge = 0;
+				striking = false;
+				canAirStrike = false;
+				
+				player.velocity = Vector2.Zero;
+				
+				if(ticks == 0)
+					Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Custom/IceBeamChargeShot"), player.position);
+				ticks++;
+				
+				radius = 8;
+				degrees += radians/2;
+				
+				Vector2 updateCenter = player.position + new Vector2(0, player.height/2);
+				
+				#region
+				// the amplitude of the wave
+				float Offset = 25.0f;
+		
+				// 360 degrees in radians
+				float Revolution = 6.28308f;
+
+				// how many waves are completed per second
+				float WavesPerSecond = 2.0f;
+
+				// get time between updates
+				float Time = 1.0f / Main.frameRate;
+				
+				// increase wave timer
+				WaveTimer += Time * Revolution * WavesPerSecond;
+				
+				float WaveOffset = (float)Math.Sin(WaveTimer) * Offset;
+				
+				float Angle = (float)Math.Atan2(Main.screenPosition.Y + Main.mouseY - updateCenter.Y,
+												Main.screenPosition.X + Main.mouseX - updateCenter.X);
+				#endregion
+				
+				if(ticks%6 == 0)
+					airStrike = Projectile.NewProjectile(updateCenter, Vector2.Zero, 134, 64, Main.projectile[134].knockBack, player.whoAmI, 0f, 0f);
+				Main.projectile[airStrike].velocity.X = (float)(radius*Math.Cos(Angle));
+				Main.projectile[airStrike].velocity.Y = (float)(radius*Math.Sin(Angle));
+				Main.projectile[airStrike].position += new Vector2(WaveOffset/2, 0);
+				
+				if(ticks > 180)
+				{
+					midAir = false;
+					ticks = 0;
+				}
+			}	
+			if(StrikeCoolDown > 0)
+				StrikeCoolDown--;
+			#endregion
+		}
+		
+		public bool CheckUp(int i, int j, Player player)
+		{
+			bool Active = Main.tile[i, j-1].active() || Main.tile[i+1, j-1].active();
+			bool Solid = Main.tileSolid[Main.tile[i, j-1].type] == true || Main.tileSolid[Main.tile[i+1, j-1].type] == true;
+			
+			if(Active && Solid) return true;
+			else return false;
 		}
 		public bool CheckLeft(int i, int j, Player player)
 		{
--- ProjectWorld.cs
+++ ProjectWorld.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Terraria;
 using Terraria.ID;
 using Terraria.Localization;
@@ -11,12 +12,130 @@ using Microsoft.Xna.Framework;
 using Terraria.GameContent.Generation;
 using Terraria.ModLoader.IO;
 using Terraria.DataStructures;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Graphics;
 
 namespace TakerylProject
 {
 	public class ProjectWorld : ModWorld
 	{
+		static Vector2 boxOrigin;
+		static Vector2 boxSize;
+		static Vector2 boxCenter;
+		// Draw before players, npcs, or projectiles
+		public override void PostDrawTiles()
+		{
+			boxOrigin = new Vector2(32, 160);
+			boxSize = new Vector2(40, 40);
+			boxCenter = new Vector2(96, 224);
+			
+			Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+			
+			if(!Main.playerInventory)
+				DrawBorderedRect(Main.spriteBatch, Color.LightGreen * 0.1f, Color.LightGreen * 0.3f, boxOrigin, boxSize, 4);
 		
+			Main.spriteBatch.End();
+		}
+		bool Spacial = false, Blink = false;
+		public void DrawBorderedRect(SpriteBatch spriteBatch, Color color, Color borderColor, Vector2 position, Vector2 size, int borderWidth)
+		{
+			var modPlayer = Main.LocalPlayer.GetModPlayer<ProjectPlayer>(mod);
+			
+			SpriteEffects effects = SpriteEffects.None;
+			
+			Color colorSpecial = default(Color);
+			Color newColor = default(Color);
+			Color newColor2 = default(Color);
+			Color newColor3 = default(Color);
+			Color newColor4 = default(Color);
+			
+			string textSize = "Right Mouse";
+			
+			int leftOrient = (int)boxCenter.X - 76;
+			int verticalOrient = (int)boxCenter.Y;
+			
+			Player player = Main.LocalPlayer;
+			
+			for(int i = 0; i < player.armor.Length -1; i++)
+			{
+				if(player.armor[i].type == mod.ItemType("SpacialAnomaly"))
+				{
+					Spacial = true;
+					break;
+				}
+				else Spacial = false;
+			}
+			for(int i = 0; i < player.armor.Length -1; i++)
+			{
+				if(player.armor[i].type == mod.ItemType("BlinkDagger"))
+				{
+					Blink = true;
+					break;
+				}
+				else Blink = false;
+			}
+			
+			// Light
+			string textSpecial = "Light (Q)";
+			if(modPlayer.lightState)
+				colorSpecial = Color.LightBlue;
+			else if(!modPlayer.lightState)
+				colorSpecial = Color.Black;
+			spriteBatch.DrawString(Main.fontMouseText, textSpecial, boxCenter + new Vector2(32, 64), colorSpecial, 0f, new Vector2(8,8) + Main.fontMouseText.MeasureString(textSize), 1f, effects, 0f);
+			
+			if(Spacial)
+			{
+				// Single Missile
+				if(modPlayer.missileCoolDown > 0)
+					newColor = Color.Red;
+				else if(modPlayer.missileCoolDown == 0)
+					newColor = Color.LightGreen;
+				
+				string text = "Right Mouse";
+				spriteBatch.DrawString(Main.fontMouseText, text, boxCenter + new Vector2(32, -64), newColor, 0f, new Vector2(8,8) + Main.fontMouseText.MeasureString(textSize), 1f, effects, 0f);
+					
+				spriteBatch.Draw(Main.magicPixel, new Rectangle(leftOrient, verticalOrient - 76, (int)100, 4), Color.Black);
+				spriteBatch.Draw(Main.magicPixel, new Rectangle(leftOrient, verticalOrient - 76, (int)modPlayer.missileCoolDown/9, 4), Color.Lerp(Color.Green, Color.Red, (float)modPlayer.missileCoolDown/900));
+				
+				// Blade Spin
+				if(modPlayer.spinCoolDown > 0)
+					newColor2 = Color.Red;
+				else if(modPlayer.spinCoolDown == 0)
+					newColor2 = Color.LightGreen;
+				
+				string text2 = "Middle Mouse";
+				spriteBatch.DrawString(Main.fontMouseText, text2, boxCenter + new Vector2(32, -32), newColor2, 0f, new Vector2(8,8) + Main.fontMouseText.MeasureString(textSize), 1f, effects, 0f);
+				
+				spriteBatch.Draw(Main.magicPixel, new Rectangle(leftOrient, (int)boxCenter.Y - 44, (int)100, 4), Color.Black);
+				spriteBatch.Draw(Main.magicPixel, new Rectangle(leftOrient, (int)boxCenter.Y - 44, (int)modPlayer.spinCoolDown/12, 4), Color.Lerp(Color.Green, Color.Red, (float)modPlayer.spinCoolDown/1200));
+			}
+			if(Blink)
+			{
+				// Air Strike
+				if(modPlayer.strikeCoolDown > 0)
+					newColor3 = Color.Red;
+				else if(modPlayer.strikeCoolDown == 0)
+					newColor3 = Color.LightGreen;
+				
+				string text3 = "'X' Key";
+				spriteBatch.DrawString(Main.fontMouseText, text3, boxCenter + new Vector2(32, 0), newColor3, 0f, new Vector2(8,8) + Main.fontMouseText.MeasureString(textSize), 1f, effects, 0f);
+
+				spriteBatch.Draw(Main.magicPixel, new Rectangle(leftOrient, (int)boxCenter.Y - 12, (int)100, 4), Color.Black);
+				spriteBatch.Draw(Main.magicPixel, new Rectangle(leftOrient, (int)boxCenter.Y - 12, (int)modPlayer.strikeCoolDown/27, 4), Color.Lerp(Color.Green, Color.Red, (float)modPlayer.strikeCoolDown/2700));
+				
+				// Blink
+				if(modPlayer.blinkCoolDown > 0)
+					newColor4 = Color.Red;
+				else if(modPlayer.blinkCoolDown == 0)
+					newColor4 = Color.LightGreen;
+				
+				string text4 = "Left-Shift";
+				spriteBatch.DrawString(Main.fontMouseText, text4, boxCenter + new Vector2(32, 32), newColor4, 0f, new Vector2(8,8) + Main.fontMouseText.MeasureString(textSize), 1f, effects, 0f);
+				
+				spriteBatch.Draw(Main.magicPixel, new Rectangle(leftOrient, (int)boxCenter.Y + 20, (int)100, 4), Color.Black);
+				spriteBatch.Draw(Main.magicPixel, new Rectangle(leftOrient, (int)boxCenter.Y + 20, (int)modPlayer.blinkCoolDown/9, 4), Color.Lerp(Color.Green, Color.Red, (float)modPlayer.blinkCoolDown/900));
+			}
+		}
 	}
 }
